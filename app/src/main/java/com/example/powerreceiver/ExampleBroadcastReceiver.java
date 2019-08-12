6dd32fa599956f0018b47598cf0098fd4b45443a
ashuhitman <ashu8306@gmail.com>
2019-08-13 04:49:20 +0530

    Detect WiFi State Changes with BroadcastReceiver

--- app/src/main/java/com/example/powerreceiver/ExampleBroadcastReceiver.java
+++ app/src/main/java/com/example/powerreceiver/ExampleBroadcastReceiver.java
@@ -3,13 +3,20 @@ package com.example.powerreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.widget.Toast;
 
 public class ExampleBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Toast.makeText(context, "Boot Completed", Toast.LENGTH_SHORT).show();
+        if (ConnectivityManager.EXTRA_NO_CONNECTIVITY.equals(intent.getAction())) {
+            boolean noConnectivity = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+            if (noConnectivity) {
+                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
+            }
+
         }
 
     }
--- app/src/main/java/com/example/powerreceiver/MainActivity.kt
+++ app/src/main/java/com/example/powerreceiver/MainActivity.kt
@@ -1,14 +1,75 @@
 package com.example.powerreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-   
+   lateinit var wifiManager: WifiManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        wifi_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                // The switch is enabled/checked
+                wifiManager.setWifiEnabled(true)
+                wifi_switch.text = "WIFI IS ON"
+            } else {
+                // The switch is disabled
+                wifiManager.setWifiEnabled(false)
+                wifi_switch.text = "WIFI IS OFF"
+
+            }
+        }
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val intentFilter = IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        registerReceiver(wifiStateReceiver, intentFilter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(wifiStateReceiver)
+    }
+
+    val wifiStateReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            var wifiStateExtra: Int = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                WifiManager.WIFI_STATE_UNKNOWN)
+
+            when(wifiStateExtra) {
+                WifiManager.WIFI_STATE_ENABLED -> {
+                    wifi_switch.isChecked = true
+                    wifi_switch.text = "WIIF IS ON"
+                }
+
+                WifiManager.WIFI_STATE_DISABLED -> {
+                    wifi_switch.isChecked = false
+                    wifi_switch.text = "WIIF IS OFF"
+                }
+            }
+
+        }
+    }
+
+
+
 }
