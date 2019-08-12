5b7734f9855cf4015991ada741098cf47a72012e
ashuhitman <ashu8306@gmail.com>
2019-08-13 03:22:36 +0530

    Static BroadcastReceiver

---
+++ app/src/main/java/com/example/powerreceiver/ExampleBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.example.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Toast.makeText(context, "Boot Completed", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
